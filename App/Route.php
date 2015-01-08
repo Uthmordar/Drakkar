8fa62bd9358ebe4da5d5dab6f6080c4596cf2fe3
Tanguy <tanguyrygodin@gmail.com>
2015-01-08 12:07:20 +0100

    remove old useless code

--- App/Route.php
+++ App/Route.php
@@ -1,10 +1,6 @@
 <?php
 namespace App;
 use vendor\Drakkar\Router\Route;
-/*use vendor\Drakkar\Router;
-
-Router::addRoute('index.php', 'Home->showHome');
-Router::addRoute('toto.php', 'Home->showHome');*/
 
 $router->addRoute(new Route(['pattern' => '\/error.php', 'connect' => 'Home:show404']));
 $router->addRoute(new Route(['pattern' => '\/', 'connect' => 'Home:showHome']));
